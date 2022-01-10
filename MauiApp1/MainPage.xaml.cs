d5c69b1607cb71bbd2393b76b162d8ebb194a624
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-01-11 00:20:39 +0100

    Update MainPage.xaml.cs

--- MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MainPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿#nullable enable
+using System;
 using System.Reflection;
 using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
 using Svg.Maui;
 
 namespace MauiApp1;
@@ -19,11 +21,30 @@ public partial class MainPage : ContentPage
         var assembly = GetType().GetTypeInfo().Assembly;
 
         _drawable = SvgDrawable.CreateFromResource(name, assembly);
-        if (_drawable is { })
+        if (_drawable?.Picture is { })
         {
             graphicsView.Drawable = _drawable;
             graphicsView.WidthRequest = _drawable.Picture.Width;
             graphicsView.HeightRequest = _drawable.Picture.Height;
+        }
+    }
+
+    private async void ButtonOpen_Clicked(object sender, EventArgs args)
+    {
+        var file = await FilePicker.PickAsync();
+        if (file is { })
+        {
+            var stream = await file.OpenReadAsync();
+            if (stream is { })
+            {
+                _drawable = SvgDrawable.CreateFromStream(stream);
+                if (_drawable?.Picture is { })
+                {
+                    graphicsView.Drawable = _drawable;
+                    graphicsView.WidthRequest = _drawable.Picture.Width;
+                    graphicsView.HeightRequest = _drawable.Picture.Height;
+                }
+            }
         }
     }
 }
